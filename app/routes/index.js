7a99f6171e1c49168d070a3186d7213d4b277a23
jackwallace180 <you@example.com>
2020-04-08 16:19:32 -0400

    added comments to relevant code

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,23 +4,29 @@ var {spawn} = require('child_process')
 router.get('/', (req,res) => {
   res.render('form', { title: 'numbers!' });
 });
+// creates a /results page after you enter a number
 router.post('/results', (req, res) => {
-
+	// store the input variable 
 	var num = req.body.number;
+	// create a function that returns the spawn of the run.py file
 	function runScript(){
 		return spawn('python3', ['./python/run.py', num]);
 	}
+	// create variables from the data above
 	var subprocess = runScript()
 	var dataToSend;
 	var runData;
 	subprocess.stdout.on('data', (data) => {
+		// show in console that numbers are being calculated (code being ran)
 		console.log("calculating numbers for :");
 		console.log(num);
 		dataToSend = data.toString();
 	});
+	// show any errors in console
 	subprocess.stderr.on('data', (data) => {
 		console.log(`error:${data}`);
 	});
+	// show when process has finished
 	subprocess.stderr.on('close', () => {
 		console.log("Closed");
 		res.render('index', { results: dataToSend })
